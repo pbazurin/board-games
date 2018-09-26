ce4181da3838ec7c63d90d91a3ac0f2497568b57
Petr Bazurin <boltik61@gmail.com>
2018-09-26 16:43:14 +0300

    Add material

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+  ],
+  declarations: [
+    AppComponent,
+    HeaderComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/client/app/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bg-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+}
