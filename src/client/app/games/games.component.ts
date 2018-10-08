47d42f73890deea6bdc15fa8de3743805f6062ff
Petr Bazurin <boltik61@gmail.com>
2018-10-08 14:09:17 +0300

    Update layout

--- src/client/app/games/games.component.ts
+++ src/client/app/games/games.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'bg-games',
-  templateUrl: './games.component.html'
+  templateUrl: './games.component.html',
+  styleUrls: ['./games.component.scss']
 })
 export class GamesComponent implements OnInit {
 
