c075cf0dd51d798633fcce6f80a6eeb31c6e2de9
Petr Bazurin <boltik61@gmail.com>
2018-11-21 17:04:31 +0200

    Restore not-found filter

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 import { config } from './config';
+import { NotFoundExceptionsFilter } from './not-found-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalFilters(new NotFoundExceptionsFilter());
   app.useStaticAssets(config.staticAssetsDirPath);
 
   await app.listen(config.port);
