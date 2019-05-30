adf7d63ebd1dcc13e6b5ac9932decb73771eeff6
Petr Bazurin <boltik61@gmail.com>
2019-05-30 16:46:01 +0300

    Update nest.js to fix vulnerability warning

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app/app.module';
 import { config } from './config';
 import { NotFoundExceptionsFilter } from './not-found-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalFilters(new NotFoundExceptionsFilter());
   app.useStaticAssets(config.staticAssetsDirPath);
